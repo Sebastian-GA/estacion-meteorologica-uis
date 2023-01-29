123e3c0e56e5085c57e7ebc2bd10d709c4f2abd6
Sebastian-GA <isebastian.ga@gmail.com>
2023-01-29 01:05:55 -0500

    getStationsInfo()

--- scripts.js
+++ scripts.js
@@ -1,24 +1,8 @@
-let stationsIDs = [];
-let stationsKeys = [];
-
-const readChannelRequest =
+let STATIONS = [];
+const url_api =
     "https://api.thingspeak.com/channels/{id}/feeds{format}?api_key={key}&results={x}";
-const readStatusRequest =
-    "https://api.thingspeak.com/channels/{id}/status.json?api_key={key}";
 
-async function readStationsFile() {
-    const response = await fetch("./stations.json");
-    const stations = await response.json();
-
-    stations.stations.forEach((element) => {
-        stationsIDs.push(element.channelID);
-        stationsKeys.push(element.readAPIKey);
-    });
-    return { stationsIDs, stationsKeys };
-}
-
-readStationsFile();
-
+// MAP SETUP
 let map = L.map("map").setView([7.1404, -73.1201], 15);
 map.doubleClickZoom.disable();
 
@@ -28,3 +12,33 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+// STATIONS
+async function getStationsInfo() {
+    const response = await fetch("./stations.json");
+    const stationsfile = await response.json();
+
+    stationsfile.stations.forEach((element) => {
+        const id = element.channelID;
+        const key = element.readAPIKey;
+
+        const response = fetch(
+            // Get other properties of station
+            `https://api.thingspeak.com/channels/${id}/feeds.json?api_key=${key}&results=0`
+        )
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                STATIONS.push({
+                    name: data.channel.name,
+                    id: data.channel.id,
+                    key: data.channel.key,
+                    location: [data.channel.latitude, data.channel.longitude],
+                    updated_at: data.channel.updated_at,
+                });
+            });
+    });
+}
+
+getStationsInfo();
