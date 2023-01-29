2c8361bec88aaaf03892ba3c5c2ccd0b955dabe8
Sebastian-GA <isebastian.ga@gmail.com>
2023-01-29 03:37:35 -0500

    select station option

--- scripts.js
+++ scripts.js
@@ -15,42 +15,68 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 // STATIONS
 async function getStationsInfo() {
+    // This function must be executed only once!
     const response = await fetch("./stations.json");
     const stationsfile = await response.json();
 
-    stationsfile.stations.forEach((element) => {
-        const id = element.channelID;
-        const key = element.readAPIKey;
+    for (let index = 0; index < stationsfile.stations.length; index++) {
+        const station = stationsfile.stations[index];
+        const id = station.channelID;
+        const key = station.readAPIKey;
 
         // Get other properties of station
-        const response = fetch(
-            `https://api.thingspeak.com/channels/${id}/feeds.json?api_key=${key}&results=0`
-        )
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                const { name, latitude, longitude, updated_at } = data.channel;
+        const response = await fetch(
+            `https://api.thingspeak.com/channels/${id}/feeds.json?api_key=${key}&results=1`
+        );
+        const data = await response.json();
+        const { name, latitude, longitude, updated_at } = data.channel;
 
-                const icon = L.icon({
-                    iconUrl:
-                        "https://icons.getbootstrap.com/assets/icons/exclamation-triangle-fill.svg",
-                    iconSize: [38, 95],
-                });
-                const marker = L.marker([latitude, longitude], { icon: icon })
-                    .addTo(map)
-                    .bindPopup(name);
+        const icon = L.icon({
+            iconUrl:
+                "https://icons.getbootstrap.com/assets/icons/exclamation-triangle-fill.svg",
+            iconSize: [38, 95],
+        });
+        const marker = L.marker([latitude, longitude], { icon: icon })
+            .addTo(map)
+            .bindPopup(name);
 
-                STATIONS.push({
-                    name: name,
-                    id: id,
-                    key: key,
-                    location: [latitude, longitude],
-                    updated_at: updated_at,
-                    marker: marker,
-                });
-            });
+        STATIONS.push({
+            name: name,
+            id: id,
+            key: key,
+            location: [latitude, longitude],
+            updated_at: updated_at,
+            last_feed: data.feeds,
+            marker: marker,
+        });
+    }
+
+    // Set options in the "select-station" selector
+    let options = "";
+    STATIONS.forEach((station) => {
+        options += `<option value="${station.location}">${station.name}</option>`;
     });
+    document.getElementById("select-station").innerHTML = options;
 }
 
 getStationsInfo();
+
+async function updateStationsStatus() {
+    // This function get the last feed (must be executed at least every minute)
+    for (let index = 0; index < STATIONS.length; index++) {
+        const station = STATIONS[index];
+        const response = await fetch(
+            `https://api.thingspeak.com/channels/${station.id}/feeds.json?api_key=${station.key}&results=1`
+        );
+        const data = await response.json();
+
+        station.last_feed = data.feeds;
+    }
+}
+
+document
+    .getElementById("select-station")
+    .addEventListener("change", function (e) {
+        let coords = e.target.value.split(",");
+        map.setView(coords);
+    });
