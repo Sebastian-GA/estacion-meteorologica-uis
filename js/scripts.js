3bd623ff0811047d5a736f880e7556a801c44a18
Sebastian-GA <isebastian.ga@gmail.com>
2023-02-09 18:28:53 -0500

    getTodayData() function

--- js/scripts.js
+++ js/scripts.js
@@ -56,10 +56,11 @@ async function setup() {
     document.getElementById("select-station").innerHTML = options;
 
     // Build Gauges
+    getTodayData();
     buildGauges();
 }
 
-async function updateStationsStatus() {
+async function updateLastFeed() {
     // This function gets the last feed
     for (let index = 0; index < STATIONS.length; index++) {
         const station = STATIONS[index];
@@ -73,15 +74,85 @@ async function updateStationsStatus() {
     }
 }
 
+// ---------------------------- Data Analysis ----------------------------
+
+Array.prototype.max = function () {
+    return Math.max.apply(null, this);
+};
+
+Array.prototype.min = function () {
+    return Math.min.apply(null, this);
+};
+
+Array.prototype.avg = function () {
+    return this.reduce(function (p, c, i, a) {
+        return p + c / a.length;
+    }, 0);
+};
+
+async function getDayData(day) {
+    // Day date in YYYY-MM-DD format
+    const dayData = [];
+    for (let index = 0; index < STATIONS.length; index++) {
+        const station = STATIONS[index];
+        const response = await fetch(
+            `https://api.thingspeak.com/channels/${station.id}/feeds.json?api_key=${station.key}` +
+                `&timezone=America%2FBogota&round=2&start=${day}%2000:00:00&end=${day}%2023:59:59`
+        );
+        const data = await response.json();
+
+        // Reformat data into arrays
+        const fields = { field1: [], field2: [], field3: [], field4: [], field5: [] };
+        const created_at = [];
+        data.feeds.forEach((entry) => {
+            created_at.push(entry.created_at);
+            for (let i = 1; i <= 5; i++) {
+                // If NaN value then add [] instead
+                if (!isNaN(+entry[`field${i}`])) {
+                    fields[`field${i}`].push(+entry[`field${i}`]);
+                } else {
+                    fields[`field${i}`].push([]);
+                }
+            }
+        });
+        // Data analysis
+        dayData.push({ field1: {}, field2: {}, field3: {}, field4: {}, field5: {} });
+        for (let i = 1; i <= 5; i++) {
+            dayData[index][`field${i}`].avg = +fields[`field${i}`].avg().toFixed(2);
+            dayData[index][`field${i}`].max = +fields[`field${i}`].max().toFixed(2);
+            dayData[index][`field${i}`].min = +fields[`field${i}`].min().toFixed(2);
+            // Include all day-feeds
+            dayData[index][`field${i}`].feeds = fields[`field${i}`];
+        }
+        // Include all day-feeds
+        dayData[index].created_at = created_at;
+    }
+    return dayData;
+}
+
+async function getTodayData() {
+    // Get today date in YYYY-MM-DD format
+    let today = new Date();
+    const offset = today.getTimezoneOffset();
+    today = new Date(today.getTime() - offset * 60 * 1000);
+    today = today.toISOString().split("T")[0];
+
+    const data = await getDayData(today);
+    for (let index = 0; index < STATIONS.length; index++) {
+        STATIONS[index].todayData = data[index];
+    }
+}
+
 function updateMap() {
     // TODO
     return;
 }
 
 function update() {
-    updateStationsStatus();
+    updateLastFeed();
     updateMap();
     updateGauges();
+    getTodayData();
 }
 
 document.getElementById("select-station").addEventListener("change", function (e) {
@@ -92,4 +163,4 @@ document.getElementById("select-station").addEventListener("change", function (e
 });
 
 setup();
-setInterval(update, 6000); // Update Status every 60 Seconds
+setInterval(update, 60000); // Update Status every 60 Seconds
